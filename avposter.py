2dadf08161a4755edc7b1a5b6fb3e0feb91285ec
midoribaka <midoribaka@users.noreply.github.com>
2015-12-03 16:35:10 +0300

    Пока не используется

--- avposter.py
+++ avposter.py
@@ -1,166 +1,268 @@
-# -*- coding: utf-8 -*-
-from selenium import webdriver
-from selenium.common.exceptions import NoSuchElementException
-from selenium.webdriver.support.ui import Select
-from PIL import Image
-import unittest
+# -*-coding: utf-8 -*-
+from grab import Grab, GrabError, DataNotFound
+from lxml import html
+import sys
+import argparse
 import os
+import logging
+
+parser = argparse.ArgumentParser(prog="avposter")
+g = Grab()
 
 
-category_id = {1: u"Автомобили",
-               2: u"Мотоциклы и мототехника",
-               3: u"Грузовики и спецтехника",
-               4: u"Водный транспорт",
-               5: u"Запчасти и аксессуары",
-               6: u"Квартиры",
-               7: u"Комнаты",
-               8: u"Дома, дачи, коттеджи",
-               9: u"Земельные участки",
-               10: u"Гаражи и машиноместа",
-               11: u"Коммерческая недвижимость",
-               12: u"Недвижимость за рубежом",
-               13: u"Вакансии",
-               14: u"Резюме",
-               15: u"Предложения услуг",
-               16: u"Запросы на услуги",
-               17: u"Одежда, обувь, аксессуары",
-               18: u"Детская одежда и обувь",
-               19: u"Товары для детей и игрушки",
-               20: u"Часы и украшения",
-               21: u"Красота и здоровье",
-               22: u"Бытовая техника",
-               23: u"Мебель и интерьер",
-               24: u"Посуда и товары для кухни",
-               25: u"Продукты питания",
-               26: u"Ремонт и строительство",
-               27: u"Растения",
-               28: u"Аудио и видео",
-               29: u"Игры, приставки и программы",
-               30: u"Настольные компьютеры",
-               31: u"Ноутбуки",
-               32: u"Оргтехника и расходники",
-               33: u"Планшеты и электронные книги",
-               34: u"Телефоны",
-               35: u"Товары для компьютера",
-               36: u"Фототехника",
-               37: u"Билеты и путешествия",
-               38: u"Велосипеды",
-               39: u"Книги и журналы",
-               40: u"Коллекционирование",
-               41: u"Музыкальные инструменты",
-               42: u"Охота и рыбалка",
-               43: u"Спорт и отдых",
-               44: u"Собаки",
-               45: u"Кошки",
-               46: u"Птицы",
-               47: u"Аквариум",
-               48: u"Другие животные",
-               49: u"Товары для животных",
-               50: u"Готовый бизнес",
-               51: u"Оборудование для бизнеса"
-               }
+def login():
+    """login to avito.ru. Login and password will get from login.cfg.
+    format login.cfg: first row - login, second - password.
+    etc:
+    admin@admin.ru
+    megapassword
+    """
+    logging.basicConfig(level=logging.DEBUG)
+    g = Grab(log_file="1.html")
+    if os.path.exists('login.cfg'):
+        with open('login.cfg') as logincfg:
+            logindata = logincfg.readlines()
+    else:
+        print "Login.cfg doesn't exist. Creating."
+        data = []
+        logindata = raw_input('Login:')
+        data.append(logindata)
+        del logindata
+        logindata = raw_input('Password:')
+        data.append(logindata)
+        with open ('login.cfg', 'w',) as logincfg:
+            for item in data:
+                print>>logincfg, item
+        with open ('login.cfg') as logincfg:
+            logindata = logincfg.readlines()
+    g.go('http://m.avito.ru/profile')
+    g.doc.set_input('login', logindata[0])
+    g.doc.set_input('password', logindata[1])
+    add_advert() # Для теста
+    g.doc.submit()
+    g.cookies.save_to_file('cookies.txt')
+    try:
+        if g.doc.select('/html/body/div[1]/div[2]/div/div/h2').text() == u'Вход':
+            raise GrabError('Wrong login/password')
+    except DataNotFound:
+        pass
 
 
-content = {"title": u"Fender Stratocaster",
-           "description": u"Fender Stratocaster 1980 года выпуска.",
-           "price": u"20000"
-           }
+def get_items(itemtype):
+    """get items from profile.
+    itemtype: 'old' for old items. 'active' for active items"""
+    result = []
+    g.go('https://www.avito.ru/profile/items/%s' % itemtype)
+    for el in g.doc.select('//*[@id="overForm"]/div/div[2]//div[@class="description"]').node_list():
+        item = el.xpath('h3/a')[0]
+        item_id = item.get('name')[5:]
+        result.append((item_id, item.text))
+    return result
 
 
-def print_categories():
-    for key in category_id:
-            print "%s - %s" % (key, category_id[key])
+def choice_items(items):
+    """promt user for choice item or items. always return list of items"""
+    selected_items = []
+    print_items(items)
+    try:
+        choice_numbers = eval(raw_input('choice items: ') + ",")
+    except StandardError:
+        print 'invalid choice number'
+        return choice_items(items)
+    if 0 in choice_numbers:
+        return main_loop()
+    print 'you was choice:'
+    try:
+        for number in choice_numbers:
+            print items[number - 1][1]
+            selected_items.append(items[number - 1])
+        return selected_items
+    except IndexError:
+        print 'invalid choice number'
+        choice_items(items)
 
 
-class Poster(unittest.TestCase):
-    def setUp(self):
-        self.driver = webdriver.Firefox()
-        self.driver.implicitly_wait(30)
-        self.base_url = "https://m.avito.ru/"
-        self.verificationErrors = []
-        self.accept_next_alert = True
+def print_items(items):
+    """print items for choise etc."""
+    teamplate = "%d)(%s) %s"
+    for number, item in enumerate(items, 1):
+        print teamplate % (number, item[0], item[1])
 
 
-    def login(self):
-        if os.path.exists('login.cfg'):
-            with open('login.cfg') as logincfg:
-                logindata = logincfg.readlines()
-        else:
-            print "Login.cfg doesn't exist. Creating."
-            data = []
-            logindata = raw_input('Login:')
-            data.append(logindata)
-            del logindata
-            logindata = raw_input('Password:')
-            data.append(logindata)
-            with open ('login.cfg', 'w',) as logincfg:
-                for item in data:
-                    print>>logincfg, item
-            with open ('login.cfg') as logincfg:
-                logindata = logincfg.readlines()
+def ids_form_settings():
+    """get items ids from file settings.cfg"""
+    try:
+        with open('settings.cfg', 'r') as settings:
+            exist_id = settings.readlines()
+            return map(lambda x: str(int(x)), exist_id)
+    except IOError:
+        return []
 
 
-    def test_poster(self):
+def save_ids(exist_ids):
+    """save items ids to file settings.cfg"""
+    exist_ids = [x + '\n' for x in exist_ids]
+    with open('settings.cfg', 'w') as settings:
+        settings.writelines(exist_ids)
+
+
+def add_to_settings(items_id):
+    exist_id = ids_form_settings()
+    for item in items_id:
+        if (item[0]) not in exist_id:
+            exist_id.append(item[0])
+    save_ids(exist_id)
+
+
+def check_id(item_id):
+    web_id = [x[0] for x in (get_items('old') + get_items('active'))]
+    if item_id not in web_id:
+        return False
+    return True
+
+
+def add_to_autopub():
+    """show all items, promt for adding to autopub list in setting.cfg"""
+    old_items = get_items('old')
+    active_items = get_items('active')
+    print "0) exit"
+    print "choise item for adding to autopub list:"
+    selected = choice_items(active_items+old_items)
+    add_to_settings(selected)
+
+
+def items_from_settings():
+    """return items from autopub list in settings.cfg file"""
+    ids = ids_form_settings()
+    all_web_items = get_items('active') + get_items('old')
+    return filter(lambda item: item[0] in ids, all_web_items)
+
+
+def remove_from_setting(items_id):
+    """remove item by id from autopub list in setting.cfg"""
+    exists_id = ids_form_settings()
+    for item_id in items_id:
         try:
-            profile = webdriver.FirefoxProfile()
-            profile.set_preference("general.useragent.override", "Mozilla/5.0 (Windows NT 10.0; WOW64; rv:42.0) Gecko/20100101 Firefox/42.0")
-            driver = webdriver.Firefox(profile)
-            driver.get(self.base_url + "/profile/login")
-            driver.find_element_by_name("login").clear()
-            driver.find_element_by_name("login").send_keys("#")
-            driver.find_element_by_name("password").clear()
-            driver.find_element_by_name("password").send_keys("#")
-            driver.find_element_by_xpath(u"//input[@value='Войти']").click()
-            driver.find_element_by_link_text(u"Подать объявление").click()
-        except NoSuchElementException:
-                driver.find_element_by_name("password").clear()
-                driver.find_element_by_name("password").send_keys("#")
-
-                # Show captcha
-                driver.save_screenshot('current_page')
-                current_page_img = Image.open('current_page')
-                w, h = current_page_img.size
-                captcha_img = current_page_img#.crop((575, 505, w-155, h-1820))
-                captcha_img.save('captcha.jpg', 'jpeg')
-                captcha = raw_input("Enter the captcha:")
-
-                driver.find_element_by_id("captcha").clear()
-                driver.find_element_by_id("captcha").send_keys(captcha)
-                driver.find_element_by_xpath(u"//input[@value='Войти']").click()
-                driver.find_element_by_link_text(u"Подать объявление").click()
-        finally:
-
-            # Show captcha
-            driver.save_screenshot('current_page')
-            current_page_img = Image.open('current_page')
-            w, h = current_page_img.size
-            captcha_img = current_page_img#.crop((575, 505, w-155, h-1820))
-            captcha_img.save('captcha.jpg', 'jpeg')
-
-            print_categories()
-            category = input("Choose category:")
-            Select(driver.find_element_by_id("category_id")).select_by_visible_text(category_id[category])
-            Select(driver.find_element_by_id("param_162")).select_by_visible_text(u"Гитары и другие струнные")
-            driver.find_element_by_id("title").clear()
-            driver.find_element_by_id("title").send_keys(content["title"])
-            driver.find_element_by_name("description").clear()
-            driver.find_element_by_name("description").send_keys(content["description"])
-            driver.find_element_by_name("price").clear()
-            driver.find_element_by_name("price").send_keys(content["price"])
-            #driver.find_element_by_id("image_upload").send_keys("picture.jpg")
-
-            captcha = raw_input("Enter the captcha:")
-
-            driver.find_element_by_id("captcha").clear()
-            driver.find_element_by_id("captcha").send_keys(captcha)
-            driver.find_element_by_xpath(u"//input[@value='Продолжить']").click()
-            driver.find_element_by_id("service_no").click()
-            driver.find_element_by_xpath(u"//input[@value='Продолжить']").click()
-            driver.find_element_by_id("service_premium").click()
-            driver.find_element_by_id("service_vip").click()
-            driver.find_element_by_id("service_highlight").click()
-            driver.find_element_by_xpath(u"//input[@value='Продолжить']").click()
+            exists_id.remove(item_id)
+        except ValueError:
+            return
+    save_ids(exists_id)
+    return True
 
 
-if __name__ == "__main__":
-    unittest.main()
+def select_to_remove():
+    """show items from autopub list, promt for removing from list and remove selected items ids"""
+    print "Choice item for remove from autopub list:"
+    settings_items = items_from_settings()
+    print "0) exit"
+    selected = choice_items(settings_items)
+    remove_from_setting([x[0] for x in selected])
+
+
+def apply_autopub():
+    settings_ids = ids_form_settings()
+    web_old_ids = [item[0] for item in get_items('old')]
+    for id_item in settings_ids:
+        if id_item in web_old_ids:
+            g.go('https://www.avito.ru/profile/items/old?item_id[]=%s&start' % id_item)
+            print "adding item with id '%s' to active list" % id_item
+
+def login_test():
+    g = Grab(log_file="1.html")
+    g.go("http://m.avito.ru/profile")
+    g.doc.set_input("login","login")
+    g.doc.set_input("password","password")
+    g.doc.submit()
+    g.cookies.save_to_file('cookies.txt')
+
+
+def list_to_utf8(seq):
+    t = ["    u'%s'" % s.encode('utf-8') for s in seq]
+    return '[\n' + ',\n'.join(t) + '\n]'
+
+
+def add_advert():
+    print("Add new advertisement.")
+    g = Grab(log_file="2.html")
+    g.load_cookies('cookies.txt')
+    g.go("http://m.avito.ru/add")
+    #login_test()
+    from selenium.webdriver import Firefox
+    from selenium.webdriver.common.keys import Keys
+    import selenium
+    from PIL import Image
+
+    browser = Firefox()
+    driver = selenium.webdriver.Firefox()
+
+    browser.get('http://m.avito.ru/profile/login')
+    driver.implicitly_wait(10)
+    elem = driver.find_element_by_css_selector(".control-self.control-self-email")
+    elem.send_keys("nilariel@gmail.com")
+    """
+    driver.find_element_by_name("password")
+    element.send_keys("ivveqaem")
+    driver.find_element_by_class_name("control-self control-self-submit button button-solid button-blue button-large")
+    driver.find_element_by_partial_link_text("Войти")
+    element.send_keys(Keys.ENTER)
+"""
+    #browser.get('http://m.avito.ru/add')
+    browser.save_screenshot('current_page')
+    current_page_img = Image.open('current_page')
+    w, h = current_page_img.size
+    captcha_img = current_page_img#.crop((575, 505, w-155, h-1820))
+    captcha_img.save('captcha.jpg', 'jpeg')
+
+
+def main_loop():
+    print "="*40
+    print "autopub list:"
+    print_items(items_from_settings())
+    print "="*40
+    actions = {'add': add_to_autopub,
+           'remove': select_to_remove,
+           'apply': apply_autopub,
+           'exit': sys.exit}
+    actions_order = ['add', 'remove', 'apply', 'exit']
+    actions_num_list = tuple(enumerate(actions_order, 1))
+    for number, action in actions_num_list:
+        print "%d) %s" % (number, action)
+    try:
+        choice = int(raw_input('choice action:'))
+        if not (1 <= choice <= len(actions_order)):
+            raise ValueError
+    except ValueError:
+        print "invalid action number"
+    else:
+        actions[actions_num_list[choice-1][1]]()
+
+    main_loop()
+
+if __name__ == '__main__':
+    if len(sys.argv) == 1:
+        login()
+        main_loop()
+    else:
+        parser.add_argument("-p", "--apply", dest="apply", action='store_true', help="apply autopub list")
+        parser.add_argument("-a", "--add", dest='ids_to_add', action='store', default=[], nargs="+",
+                            help='add ids to autopub list')
+        parser.add_argument("-r", "--remove", dest='ids_to_remove', action='store', default=[], nargs="+",
+                            help='remove ids from autopub list')
+        namespace = parser.parse_args(sys.argv[1:])
+        for item_id in namespace.ids_to_add + namespace.ids_to_remove:
+            if not item_id.isdigit():
+                print 'ERROR: invalid id "%s"' % item_id
+                sys.exit(1)
+        checked_ids = []
+        login()
+        for item_id in namespace.ids_to_add:
+            if check_id(item_id):
+                checked_ids.append(item_id)
+            else:
+                print "id '%s' dont exist in avito" % item_id
+
+        add_to_settings(checked_ids)
+        remove_from_setting(namespace.ids_to_remove)
+
+        if namespace.apply:  # if -p, -apply apply autpub list
+            apply_autopub()
