f008c5ba563591456d2daa5a190584baa8a01693
midoribaka <midoribaka@users.noreply.github.com>
2015-12-13 16:59:31 +0300

    Разобраться с парсером

--- parser.py
+++ parser.py
@@ -1,12 +1,9 @@
 # coding: utf-8
-import urllib
-import csv
+
+"""
 import logging
-
-from grab.spider import Spider, Task
-from weblib.error import DataNotFound
-
-from avposter import list_to_utf8
+from grab import Grab
+from grab.error import DataNotFound
 
 links = ["http://lad-rostov.ru/shop/pilnye-diski-almaznye-diski/",
          "http://lad-rostov.ru/shop/sadovo-uborochnyj-instrument/",
@@ -16,47 +13,73 @@ links = ["http://lad-rostov.ru/shop/pilnye-diski-almaznye-diski/",
          "http://lad-rostov.ru/shop/ruchnoi-instrument/"
         ]
 
-class ExampleSpider(Spider):
-    # Список страниц, с которых Spider начнёт работу
-    # для каждого адреса в этом списке будет сгенерировано
-    # задание с именем initial
-    base_url = "http://lad-rostov.ru"
-    category = input("Choose category:")
-    initial_urls = [links[category]]
 
-    def prepare(self):
-        # Подготовим файл для записи результатов
-        # Функция prepare вызываетя один раз перед началом
-        # работы парсера
+# Список страниц, с которых Spider начнёт работу
+# для каждого адреса в этом списке будет сгенерировано
+# задание с именем initial
+base_url = "http://lad-rostov.ru"
+category = input("Choose category:")
+initial_urls = [links[category]]
 
-        self.result_file = csv.writer(open('result.txt', 'w'))
-    def task_initial(self, grab, task):
+g = Grab()
 
-        # Это функция-обработчик для заданий с именем initial
-        # т.е. для тех заданий, что были созданы для
-        # адреов указанных в self.initial_urls
+def task_initial():
+    # Это функция-обработчик для заданий с именем initial
+    # т.е. для тех заданий, что были созданы для
+    # адреов указанных в self.initial_urls
+    g.go(initial_urls)
+    for elem in g.doc.select('.//*[@id="context_menu"]//a'):
+        print elem.attr("href")
 
-        for elem in grab.xpath_list('.//*[@id="context_menu"]//a'):
-            yield Task('parse', url=elem.get('href'))
 
-    def task_parse(self, grab, task):
-        try:
-             grab.xpath_list('.//*[@sid="context_menu"]//a')
-
-        except DataNotFound:
-            print("Exception")
-
-        else:
-            print 'Link is: %s' % task.url
-
-            post = {
-                'url': task.url,
-                'title': grab.doc.select('.//*[@id="context_menu"]//a').text(),
+def task_parse():
+    g.xpath_list('.//*[@id="context_menu"]//a')
+    if g.response.error_code:
+        print("Request failed")
+    else:
+        print 'Link is: %s' % task.url
+        post = {
+            'url': task.url,
+            'title': g.doc.select('.//*[@id="context_menu"]//a').text(),
             }
-            print(post)
+        print(post)
 
 
 if __name__ == '__main__':
     logging.basicConfig(level=logging.DEBUG)
-    bot = ExampleSpider(thread_number=2)
-    bot.run()
+    task_initial
+"""
+
+from grab import Grab
+import logging
+
+
+g = Grab()
+links = ["http://lad-rostov.ru/shop/pilnye-diski-almaznye-diski/",
+         "http://lad-rostov.ru/shop/sadovo-uborochnyj-instrument/",
+         "http://lad-rostov.ru/shop/elektroinstrument1/",
+         "http://lad-rostov.ru/shop/zamochno-skobianye-izdelia/",
+         "http://lad-rostov.ru/shop/abrazivy-elektrody-otreznye-krugi/",
+         "http://lad-rostov.ru/shop/ruchnoi-instrument/"
+        ]
+
+category = input("Choose category:")
+
+def parser():
+    g.go(links[category])
+    if g.doc.select(".//*[@id='dContent']/h1").exists():
+        print("Exists")
+    else:
+        for elem in g.doc.select('.//*[@id="context_menu"]//a'):
+            href = elem.attr("href")
+            print href
+            print 'Link is: %s' % href
+            post = {
+                'url': href,
+                'title': g.doc.select('.//*[@id="context_menu"]//a').text(),
+                }
+            print(post)
+
+if __name__ == '__main__':
+    logging.basicConfig(level=logging.DEBUG)
+    parser()
